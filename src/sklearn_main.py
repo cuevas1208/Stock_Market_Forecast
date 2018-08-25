07af11dcffc89483776f34191ed9dfc32282eee2
cuevas1208 <cuevas1208@gmail.com>
2018-08-24 23:05:55 -0500

    cleaning code and organizing packages

--- src/sklearn_main.py
+++ src/sklearn_main.py
@@ -5,44 +5,41 @@
 # referene  https://pythonprogramming.net/machine-learning-stock-prices-python-programming-for-finance/
 ################################################################################
 import numpy as np
-from data_Load import dataPipeline, combineCSV
-import datetime as dt
-from datetime import datetime, timedelta
-from matplotlib import style
+from datetime import datetime
 import matplotlib.pyplot as plt
-import pandas as pd
-import bs4 as bs
 from collections import Counter
-import pickle, requests
 from sklearn import svm, cross_validation, neighbors
 from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 import argparse
 import logging
+
+from data_functions.data_Load import dataPipeline, combineCSV
+
 logger = logging.getLogger()
 
 PREDICTION_DAYS = 7
-DATASET_LEN = 10
+DATA_SET_LEN = 10
+THRESHOLD = 0.02
 
 stockToPredict = 'TSLA'
-ticker = stockToPredict +'_High'
+ticker = stockToPredict + '_High'
 
-treshold = 0.02
 
 ##########################################################
-#Data correlation
+# Data correlation
 ##########################################################
 def detaCorrelation(df, stocksName):
     # before doing data correlation it would be good to prepare that data
     # by taking the % differences of the stock marquet
     df = process_data_for_labels(df)
-    
+
     df_corr = df.corr()
-    #print("correlation: ") 
-    #print(df_corr)
+    # print("correlation: ")
+    # print(df_corr)
 
     data = df_corr.values
     fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
+    ax = fig.add_subplot(1, 1, 1)
     heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn)
     fig.colorbar(heatmap)
     ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
@@ -54,10 +51,11 @@ def detaCorrelation(df, stocksName):
 
     ax.set_xticklabels(column_labels)
     ax.set_yticklabels(row_labels)
-    plt.xticks(rotation = 90)
-    heatmap.set_clim(-1,1)
+    plt.xticks(rotation=90)
+    heatmap.set_clim(-1, 1)
     plt.tight_layout()
     plt.show()
+
 
 ###################################################################################
 # process_data_for_labels
@@ -66,31 +64,31 @@ def detaCorrelation(df, stocksName):
 # by shifting the columns to PREDICTION_DAYS
 ###################################################################################
 def process_data_for_labels(ticker, df):
-
-    #delete all NAN array with rows deleted
-    df.dropna(subset=[ticker],inplace=True)
+    # delete all NAN array with rows deleted
+    df.dropna(subset=[ticker], inplace=True)
     logging.debug("df shape after eliminating NAN from labels", df.shape)
 
-    #get future labels for classification
+    # get future labels for classification
     df['{}_labels'.format(ticker)] = \
-    (df[ticker].shift(-PREDICTION_DAYS) - df[ticker]) / df[ticker]
+        (df[ticker].shift(-PREDICTION_DAYS) - df[ticker]) / df[ticker]
 
-    #return a list of labels with no NAN
-    df.dropna(subset=['{}_labels'.format(ticker)],inplace=True)
+    # return a list of labels with no NAN
+    df.dropna(subset=['{}_labels'.format(ticker)], inplace=True)
     logging.debug("df shape after eliminating NAN from labels", df.shape)
 
-    logging.debug("labels with days difference",df['{}_labels'.format(ticker)] )
+    logging.debug("labels with days difference", df['{}_labels'.format(ticker)])
 
-    #transform labels in to 1, 0 and -1 
-    df['{}_labels'.format(ticker)] = list(map( buy_sell_hold, df['{}_labels'.format(ticker)]))
-    logging.debug("labels after the one_hot",df['{}_labels'.format(ticker)])
+    # transform labels in to 1, 0 and -1
+    df['{}_labels'.format(ticker)] = list(map(buy_sell_hold, df['{}_labels'.format(ticker)]))
+    logging.debug("labels after the one_hot", df['{}_labels'.format(ticker)])
 
-    #visulaize the spred
+    # visulaize the spred
     vals = df['{}_labels'.format(ticker)].values.tolist()
     str_vals = [str(i) for i in vals]
-    print('Data spread:',Counter(str_vals), "total labels: ", len(vals))
-        
+    print('Data spread:', Counter(str_vals), "total labels: ", len(vals))
+
     return df
+
 
 ###################################################################################
 # set the classifier
@@ -98,8 +96,10 @@ def process_data_for_labels(ticker, df):
         -1  = decrees
         0   = stayed same
         -1  = increased                    '''
+
+
 ###################################################################################
-def buy_sell_hold(*args, treshold = 0.02):
+def buy_sell_hold(*args, treshold=0.02):
     cols = [c for c in args]
     for col in cols:
         if col > treshold:
@@ -108,89 +108,87 @@ def buy_sell_hold(*args, treshold = 0.02):
             return -1
     return 0
 
+
 ###################################################################################
 # extract_featuresets
 ###################################################################################
 def extract_featuresets(ticker, df):
     tickers = df.columns.values.tolist()
 
-    #handles invalid numbers
+    # handles invalid numbers
     df.fillna(0, inplace=True)
     df = df.replace([np.inf, -np.inf], np.nan)
     df.dropna(inplace=True)
 
-    #tickers == to all the column labels used for features excluding labels
-    #pct_change() normilize the value to be percente chage from yesterday
+    # tickers == to all the column labels used for features excluding labels
+    # pct_change() normilize the value to be percente chage from yesterday
     df_vals = df[[ticker for ticker in tickers]].pct_change()
     df_vals = df_vals.replace([np.inf, -np.inf], 0)
     df_vals.fillna(0, inplace=True)
     print("x features shape:  ", df_vals.shape)
-    
+
     X = df_vals.values
     y = df['{}_labels'.format(ticker)].values
-    
-    return X,y,df
+
+    return X, y, df
 
 
 ###################################################################################
 # trainData
 ###################################################################################
 def do_ml(X, y):
-    
     logging.debug("X sample: \ {} ".format(len(X.shape)))
 
     logging.debug("y sample: \ {} ".format(len(y.shape)))
 
     X_train, X_test, y_train, y_test = cross_validation.train_test_split( \
-        X , y, test_size=0.25)
-    
-    #combine the predictions of several base estimators
-    clf = VotingClassifier([('lsvc',svm.LinearSVC()),
-                            ('knn',neighbors.KNeighborsClassifier()),
-                            ('rfor',RandomForestClassifier())])
+        X, y, test_size=0.25)
+
+    # combine the predictions of several base estimators
+    clf = VotingClassifier([('lsvc', svm.LinearSVC()),
+                            ('knn', neighbors.KNeighborsClassifier()),
+                            ('rfor', RandomForestClassifier())])
 
     clf.fit(X_train, y_train)
 
     y_pred = clf.predict(X_test)
     np.set_printoptions(precision=2)
-    print(np.around((y_pred - y_test), 2))
-
+    print('0=>correct,   1,2=>error')
+    print(np.abs(np.around((y_pred - y_test), 2)))
     confidence = clf.score(X_test, y_test)
-    print('accuracy:',confidence)
-    predictions = clf.predict(X_test)
-    print('predicted class counts:',Counter(predictions))
-    print()
-
+    print('accuracy:', confidence)
     return confidence
 
-def method1 (stockName, df_features):
+
+def method1(stockName, df_features):
     X, y, df = extract_featuresets(stockName, df_features)
     do_ml(X, y)
 
-def method2 (stockName, df):
-    #prepare label
+
+def method2(stockName, df):
+    # prepare label
     y = df['{}_labels'.format(ticker)].values
 
-    #ToDo: how to consider the relevant data to train on 
-    #detaCorrelation(df, stocksName)
-    #do_ml(df[stockToPredict]['Close'], df_features)
+    # ToDo: how to consider the relevant data to train on
+    # detaCorrelation(df, stocksName)
+    # do_ml(df[stockToPredict]['Close'], df_features)
 
-    #prepare X
+    # prepare X
     df['x_features'] = df[stockToPredict + '_Low'] + df[stockToPredict + '_High'] / 2
 
-    #pct_change() normalize the value to be percent change from yesterday
+    # pct_change() normalize the value to be percent change from yesterday
     df['x_features'] = df['x_features'].pct_change()
 
-    #creating features for prediction 
-    x = df['x_features'].values 
-    for i in range(1, DATASET_LEN):
+    # creating features for prediction
+    x = df['x_features'].values
+    for i in range(1, DATA_SET_LEN):
         x = np.dstack((x, df['x_features'].shift(-i).values))
 
-    print (x[0])
+    print(x[0])
     x = x[0]
     mask = ~np.isnan(x).any(axis=1)
 
-    #clean dataset
+    # clean dataset
     x = x[mask]
     y = y[mask]
 
@@ -198,50 +196,46 @@ def method2 (stockName, df):
     logging.debug(y.shape)
 
     do_ml(x, y)
-    ###########################################################
 
-###################################################################################
-# main
-###################################################################################
+
 if __name__ == "__main__":
-    import sys
+    import warnings, sys
+    if not sys.warnoptions:
+        warnings.simplefilter("ignore")
+
     # Instantiate the parser
     # logging.basicConfig(filename='log.log', level=logging.INFO)
     parser = argparse.ArgumentParser(description='Optional app description')
 
-    #Debug
+    # Debug
     parser.add_argument('-v', "--verbose", action='store_true',
                         help='Type -v to do debugging')
 
     args = parser.parse_args()
 
-    if(args.verbose):
-        logger.setLevel(logging.DEBUG)
+    if args.verbose: logger.setLevel(logging.DEBUG)
     '''
     emaples
     #logging.info('So should this')
     #logging.warning('And this, too')
     '''
-    #################################################################################
     dataDates = []
     dataDates.append('2010-01-01')
     dataDates.append(datetime.now().date())
-    
-    #from csv file with all the stocks from sp500
-    #dataPipeline returns a data set to train with 5 random stocks
+
+    # from csv file with all the stocks from sp500
+    # dataPipeline returns a data set to train with 5 random stocks
     df = dataPipeline(dataDates, stockToPredict)
     df_features, stocksName = combineCSV(df, df.keys())
 
     if (logger.getEffectiveLevel() == logging.DEBUG):
         logging.debug("Loaded most resent sample: \ {} ".format(df_features.tail(2)))
-        df_features[stockToPredict+'_High'].plot()
+        df_features[stockToPredict + '_High'].plot()
         plt.show()
 
-    #change values to persentage of change for the days intended to predict
+    # change values to percentage of change for the days intended to predict
     df = process_data_for_labels(ticker, df_features)
 
-    #method1(ticker, df)
-
+    # method1(ticker, df)
     method2(ticker, df)
     ###############################################################################
-
